ce416eef9d7a9cee1901f63e74caa57d86b2ef31
itzisraelone@gmail.com <itzisraelone@gmail.com>
2021-10-05 15:34:55 -0500

    Created Carousel Component for home section

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,9 +1,28 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import Slider from "./Slider/slider";
 
-const Home = () => {
-  return <Slider></Slider>;
-};
 
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import Banner from '../../assets/images/sliderImage/banner.jpg'
+const Home=()=>{
+            return (
+            <Carousel showThumbs={false} infiniteLoop= {true}>
+                <div>
+                    <img src={Banner} />
+                    <p className="legend">Legend 1</p>
+                </div>
+                <div>
+                    <img src={Banner} />
+                    <p className="legend">Legend 2</p>
+                </div>
+                <div>
+                    <img src={Banner} />
+                    <p className="legend">Legend 3</p>
+                </div>
+            </Carousel>
+        );
+    };
 export default Home;
+
+// ReactDOM.render(<DemoCarousel />, document.querySelector('.demo-carousel'));
